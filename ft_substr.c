643fb242d5ec6d1355613efa9ade50b6faa17c2e
Shemika Powers <spowers@student.21-school.ru>
2020-05-30 14:44:07 +0300

    ok

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: spowers <spowers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/24 01:24:46 by spowers           #+#    #+#             */
-/*   Updated: 2020/05/25 18:26:16 by spowers          ###   ########.fr       */
+/*   Updated: 2020/05/30 14:42:15 by spowers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,23 @@ char				*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	size_t	i;
+	size_t	length;
 
 	i = 0;
 	if (!s)
 		return (NULL);
-	if (start > ft_strlen(s))
+	length = ft_strlen(s);
+	if (start > length)
 		return (ft_strdup(""));
+	if (length - start < len)
+		len = length - start;
 	str = (char *)malloc(sizeof(char) * len + 1);
 	if (str == NULL)
 		return (NULL);
-	while (i < len)
+	while (s[start] != '\0' && i < len)
 	{
-		str[i] = s[i + start];
+		string[i] = s[start];
+		start++;
 		i++;
 	}
 	str[i] = '\0';
